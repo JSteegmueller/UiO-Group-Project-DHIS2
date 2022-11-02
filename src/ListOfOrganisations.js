83b7f30837b9afecf064a92b34b0c76623827f9c
Naomi <naomi.schoppa@student.htw-berlin.de>
2022-11-02 21:32:15 +0100

    feature 10 / half working compoment - just get errors on the first fetch

--- src/ListOfOrganisations.js
+++ src/ListOfOrganisations.js
@@ -11,6 +11,12 @@ import {
   CircularLoader,
 } from "@dhis2/ui";
 
+const orgUnitId = "Tht0fnjagHi";
+const lifeCommodities = "ULowA8V3ucd";
+const period = "202110";
+const categoryOptionComboEndBalance = "J2Qf1jtZuj8";
+
+// Fetching organisation unit ID's
 const organisationUnits = {
   organisationList: {
     resource: "organisationUnits",
@@ -21,39 +27,88 @@ const organisationUnits = {
   },
 };
 
+// Fetching values based on organisation ID's
 const organisationUnitsValues = {
   valueList: {
-    resource: "dataValueSets",
-    params: ({orgId}) => ({
-      dataSet: "ULowA8V3ucd",
-      period: "202110",
-      orgUnit: orgId
+    resource: "/dataValueSets",
+    params: ({ orgId }) => ({
+      dataSet: lifeCommodities,
+      period: period,
+      orgUnit: orgId,
+      fields: "dataValues[dataElement,categoryOptionCombo,value]",
     }),
   },
 };
 
-function fetchingValueIdHelper(data) {
+// Fetches all the necessary data from the organisation
+function fetchingOrgDataHelper(data) {
+  let organisationChildren = [];
+  let organisationChildrensAll = [];
   if (data) {
-    let organisationChildren = [];
     for (let item of data.organisationList.children)
-      organisationChildren.push(item.id);
+      if (orgUnitId !== item.id) {
+        organisationChildren.push(item.id);
+        organisationChildrensAll.push({
+          orgUnitName: item.name,
+          orgUnitId: item.id,
+        });
+      }
 
-    return organisationChildren;
+    return { organisationChildren, organisationChildrensAll };
   }
-  return;
+  return { organisationChildren, organisationChildrensAll };
 }
 
-function ListOfOrganisations() {
+// Fetches all the necessary data from the value and collect it in one result list
+function fetchingValueDataHelper(
+  valueOfStock,
+  requestedCommodity,
+  organisationData
+) {
+  let result = [];
+  if (valueOfStock.data) {
+    let dataValues = valueOfStock.data.valueList.dataValues;
+    let collectValues = [];
+
+    for (let item of dataValues) {
+      if (
+        item.categoryOptionCombo === categoryOptionComboEndBalance &&
+        item.dataElement === requestedCommodity
+      ) {
+        collectValues.push({
+          orgUnitId: item.orgUnit,
+          value: item.value,
+        });
+      }
+    }
+
+    result = collectValues.map((values) => ({
+      ...values,
+      ...organisationData.find(
+        (organisationName) => organisationName.orgUnitId === values.orgUnitId
+      ),
+    }));
+
+    return result;
+  }
+  return result;
+}
+
+// Later call function via requestedCommodity
+function ListOfOrganisations(/*requestedCommodity*/) {
   const { loading, error, data } = useDataQuery(organisationUnits);
-  let organisationChildren = fetchingValueIdHelper(data);
+  let organisationIds = data
+    ? fetchingOrgDataHelper(data).organisationChildren
+    : [];
+
+  // Delete this line after call was setup
+  const requestedCommodity = "dY4OCwl0Y7Y";
 
   const valueOfStock = useDataQuery(organisationUnitsValues, {
     variables: {
-      orgId: organisationChildren,
+      orgId: organisationIds,
     },
   });
-
-  console.log(valueOfStock);
 
   if (error) {
     return <span>ERROR: {error.message}</span>;
@@ -64,6 +119,24 @@ function ListOfOrganisations() {
   }
 
   if (data) {
+    let organisationData =
+      typeof fetchingOrgDataHelper(data).organisationChildrensAll ===
+      "undefined"
+        ? []
+        : fetchingOrgDataHelper(data).organisationChildrensAll;
+    const result =
+      typeof fetchingValueDataHelper(
+        valueOfStock,
+        requestedCommodity,
+        organisationData
+      ) === "undefined"
+        ? []
+        : fetchingValueDataHelper(
+            valueOfStock,
+            requestedCommodity,
+            organisationData
+          );
+
     return (
       <div>
         <h1>List of available commodity stock in other organisations</h1>
@@ -74,20 +147,20 @@ function ListOfOrganisations() {
               <TableCellHead>Amount of avialable stock</TableCellHead>
             </TableRowHead>
           </TableHead>
-          <TableBody></TableBody>
+          <TableBody>
+            {result.map(({ orgUnitName, value }, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell>{orgUnitName} </TableCell>
+                  <TableCell>{value}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
         </Table>
       </div>
     );
   }
 }
-/*
-           {orgIdArray.map(({ name, stock }, index) => {
-              return (
-                <TableRow key={index}>
-                  <TableCell>{name} </TableCell>
-                  <TableCell>{stock}</TableCell>
-                </TableRow>
-              );
-            })}*/
 
 export default ListOfOrganisations;
