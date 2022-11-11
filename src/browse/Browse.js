71448c9ac0b1a8f78ac3ea79e96174fb30651886
Kevin <vanquel12@gmail.com>
2022-11-11 13:30:13 +0100

    feat: Balance updates automatically on new month

--- src/browse/Browse.js
+++ src/browse/Browse.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { StockTable } from "./StockTable";
-import { UpdateBalanceButton } from "./UpdateBalanceButton";
-import { useDataQuery } from "@dhis2/app-runtime";
+import { useDataQuery, useDataMutation } from "@dhis2/app-runtime";
 import { CircularLoader } from "@dhis2/ui";
 
 const date = new Date();
@@ -18,6 +17,15 @@ const request = {
             orgUnit: process.env.REACT_APP_ORGUNIT,
             dataSet: "ULowA8V3ucd",
             period: currentPeriod,
+            fields: "dataValues[dataElement,categoryOptionCombo,value]",
+        },
+    },
+    previousValues: {
+        resource: "/dataValueSets",
+        params: {
+            orgUnit: process.env.REACT_APP_ORGUNIT,
+            dataSet: "ULowA8V3ucd",
+            period: previousPeriod,
             fields: "dataValues[dataElement,categoryOptionCombo,value]",
         },
     },
@@ -46,6 +54,23 @@ const request = {
     },
 };
 
+const mutateLastUpdated = {
+    resource: "/dataStore/IN5320-G3/lastUpdated",
+    data: currentPeriod,
+    type: "update",
+};
+
+const mutateBalance = {
+    resource: "/dataValueSets",
+    dataSet: "ULowA8V3ucd",
+    data: ({ updatedBalance }) => ({
+        orgUnit: process.env.REACT_APP_ORGUNIT,
+        period: currentPeriod,
+        dataValues: updatedBalance,
+    }),
+    type: "create",
+};
+
 function getPeriod(date) {
     return date.getFullYear().toString() + ("0" + (date.getMonth() + 1)).slice(-2);
 }
@@ -59,6 +84,27 @@ function getTimeDiff(date) {
 
 export function Browse({ requestHandler }) {
     const { loading, error, data, refetch } = useDataQuery(request);
+    const [mutateB, { loading1, error1 }] = useDataMutation(mutateBalance);
+    const [mutateL, { loading2, error2 }] = useDataMutation(mutateLastUpdated);
+    const [lock, setLock] = useState(false);
+
+    const updateBalance = async (data) => {
+        const updatedBalance = data.previousValues.dataValues
+            .filter((i) => i.categoryOptionCombo == "rQLFnNXXIL0")
+            .map((i) => ({
+                dataElement: i.dataElement,
+                categoryOptionCombo: i.categoryOptionCombo,
+                value: i.value,
+            }));
+        await mutateB({
+            currentPeriod: currentPeriod,
+            updatedBalance: updatedBalance,
+        });
+        await mutateL({
+            currentPeriod: currentPeriod,
+        });
+        refetch();
+    };
 
     if (error) {
         return <span>ERROR: {error.message}</span>;
@@ -69,6 +115,10 @@ export function Browse({ requestHandler }) {
     }
 
     if (data) {
+        if (!lock && data.lastUpdated != currentPeriod) {
+            updateBalance(data);
+            setLock(true);
+        }
         return (
             <div>
                 <p>User: {data.user.name}</p>
@@ -77,13 +127,7 @@ export function Browse({ requestHandler }) {
                 <p>Current Month: {currentPeriod}</p>
                 <p>Current Date: {date.toJSON().split("T")[0]}</p>
                 <p>Days until next restock: {nextRestock}</p>
-                {data.lastUpdated != currentPeriod && (
-                    <UpdateBalanceButton
-                        refetch={refetch}
-                        currentPeriod={currentPeriod}
-                        previousPeriod={previousPeriod}
-                    />
-                )}
+                {lock && <p>Balance updated for the new month!</p>}
                 <StockTable
                     key={data.lastUpdated}
                     data={data}
